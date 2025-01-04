eb2133fb89d4478be789f4ebe651fcd2d18ebe8e
Shradha Verma <shradhaverma001@gmail.com>
2025-01-04 16:37:30 +0530

    setting tailwind and basic components

---
+++ netflix-gpt/src/components/Browse.js
@@ -0,0 +1,7 @@
+const Browse = () => {
+    return (
+        <div>Browse1212hgh</div>
+    )
+}
+
+export default Browse;
---
+++ netflix-gpt/src/components/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header';
+
+const Login = () => {
+  return (
+    <div>
+        <Header />
+        <div>
+        <img src="https://assets.nflxext.com/ffe/siteui/vlv3/aa9edac4-a0e6-4f12-896e-32c518daec62/web/IN-en-20241223-TRIFECTA-perspective_1502c512-be5f-4f14-b21a-e3d75fe159ab_large.jpg" alt="net back img" />
+        </div>
+    </div>
+  )
+}
+
+export default Login;
